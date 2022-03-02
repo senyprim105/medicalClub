c4503c09b2e449c941dfdca9ad98073419da4826
senyprim105 <senyprim105@gmail.com>
2022-03-02 11:31:04 +1000

    fix select in price section

--- src/js/main.js
+++ src/js/main.js
@@ -27,7 +27,6 @@
     }
   }
   function select() {
-    console.log("select start");
     new Selectr(document.getElementById("filials"), {
       searchable: false,
       renderOption: renderer,
@@ -58,6 +57,27 @@
         data.dataset.address,
         "</span></div>",
       ];
+      return template.join("");
+    }
+  }
+  function selectInPriceSection(){
+    new Selectr(document.querySelector(".prices__select"), {
+      searchable: false,
+      renderOption: renderer,
+      renderSelection: rendererSelected,
+      customClass:"prices__select",
+    });
+    function renderer(data) {
+      var text = data.text;
+      var template = [
+        '<div class="prices__select-option">',text,'</div>'];
+      return template.join("");
+    }
+
+    function rendererSelected(data) {
+      var text = data.text;
+      var template = [
+        '<div class="prices__select--selected">',text,'</div>'];
       return template.join("");
     }
   }
@@ -259,6 +279,7 @@
     sliderStaff();
     inputMask();
     select();
+    selectInPriceSection();
     // ymaps.ready(initMap);
   });
 })();
