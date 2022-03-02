79cfcd3e1605c3910fff94aa74f0109420a6f18e
senyprim105 <senyprim105@gmail.com>
2022-03-03 01:33:21 +1000

    fix colorfull input

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,14 @@
 
 (function () {
   function inputMask() {
-    const name = document.querySelector("#feedback__phone");
-    if (name) {
-      console.log(name);
-      Inputmask({ mask: "+7 (999) 999-99-99" }).mask(name);
+    const feedbackPhone = document.querySelector("#feedback__phone");
+    if (feedbackPhone) {
+      Inputmask({ mask: "+7 (999) 999-99-99" }).mask(feedbackPhone);
+    }
+
+    const questionPhone = document.querySelector("#questions__form-input--phone");
+    if (questionPhone) {
+      Inputmask({ mask: "+7 (999) 999-99-99" }).mask(questionPhone);
     }
   }
   function menu() {
